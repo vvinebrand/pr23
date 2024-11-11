380684f5cb872d73672f15e7299bc1effa933e98
vvinebrand <vvinebrand@gmail.com>
2024-11-11 13:48:36 +0700

    add SplashScreen

--- app/src/main/java/com/example/pr23/MainActivity.kt
+++ app/src/main/java/com/example/pr23/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -13,35 +14,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.pr23.ui.theme.PR23Theme
 
-class MainActivity : ComponentActivity() {
+
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            PR23Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PR23Theme {
-        Greeting("Android")
+        setContentView(R.layout.main_activity)
     }
 }
---
+++ app/src/main/java/com/example/pr23/splash_screen.java
@@ -0,0 +1,6 @@
+package com.example.pr23;
+
+import android.app.Activity;
+
+public class splash_screen extends Activity {
+}
